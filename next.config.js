e1f8deeccda03a66662c972fe367c6c8cad2efb4
Rajat <rajat@nevrio.tech>
2023-03-06 15:52:55 +0530

    next.configa55

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 // const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
-
+module.exports = {};
 // module.exports = (phase) => {
 //     if (phase ===PHASE_DEVELOPMENT_SERVER){
 //         return {
